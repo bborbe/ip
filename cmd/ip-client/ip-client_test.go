409e3e8a84c96bd2a9ac40708201500b4d94a08d
Benjamin Borbe <bborbe@rocketnews.de>
2018-06-21 11:03:31 +0200

    print url missing

--- cmd/ip-client/ip-client_test.go
+++ cmd/ip-client/ip-client_test.go
@@ -7,14 +7,15 @@ import (
 	"testing"
 	"time"
 	"os/exec"
+	"github.com/onsi/gomega/gbytes"
 )
 
-var pathToServerBinary string
-var serverSession *gexec.Session
+var pathToBinary string
+var session *gexec.Session
 
 var _ = BeforeSuite(func() {
 	var err error
-	pathToServerBinary, err = gexec.Build("github.com/bborbe/ip/cmd/ip-client")
+	pathToBinary, err = gexec.Build("github.com/bborbe/ip/cmd/ip-client")
 	Expect(err).NotTo(HaveOccurred())
 })
 
@@ -29,10 +30,18 @@ func TestIpClient(t *testing.T) {
 
 var _ = Describe("ip-client", func() {
 	var err error
-	It("returns with exitcode != 0 if no parameters have been given", func() {
-		serverSession, err = gexec.Start(exec.Command(pathToServerBinary), GinkgoWriter, GinkgoWriter)
-		Expect(err).To(BeNil())
-		serverSession.Wait(time.Second)
-		Expect(serverSession.ExitCode()).To(Equal(1))
+	Context("without parameters", func() {
+		It("returns with exitcode != 0", func() {
+			session, err = gexec.Start(exec.Command(pathToBinary), GinkgoWriter, GinkgoWriter)
+			Expect(err).To(BeNil())
+			session.Wait(time.Second)
+			Expect(session.ExitCode()).To(Equal(1))
+		})
+		It("returns with exitcode != 0", func() {
+			session, err = gexec.Start(exec.Command(pathToBinary), GinkgoWriter, GinkgoWriter)
+			Expect(err).To(BeNil())
+			session.Wait(time.Second)
+			Expect(session.Err).To(gbytes.Say("parameter url missing"))
+		})
 	})
 })
